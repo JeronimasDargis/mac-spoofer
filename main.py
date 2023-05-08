d683f1c65943488726132844dbfcb035bf95c21d
jeronimasdargis <jeronimasd@gmail.com>
2023-05-08 09:12:12 -0400

    feat: capturing user input through command line arguments and handling error

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
 import subprocess
+import optparse
 
-interfaceCard = input("interface > ")
-newMac = input("new MAC > ")
 
-information = "[+] Changing MAC address for: {} to: {}".format(interfaceCard, newMac)
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface_card", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface_card:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    if not options.new_mac:
+        parser.error("[-] Please specify an new MAC, use --help for more info.")
+    return options
 
-print(information)
 
-# subprocess.call("ifconfig " + interfaceCard + " down", shell=True)
-# subprocess.call("ifconfig " + interfaceCard + " hw ether " + newMac, shell=True)
-# subprocess.call("ifconfig " + interfaceCard + " up", shell=True)
+def change_mac(interface_card, new_mac):
+    information = "[+] Changing MAC address for: {} to: {}".format(interface_card, new_mac)
+    print(information)
+    subprocess.call(["ifconfig", interface_card, "down"])
+    subprocess.call(["ifconfig", interface_card, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface_card, "up"])
+
+
+user_input = get_arguments()
+change_mac(user_input.interface_card, user_input.new_mac)
+
